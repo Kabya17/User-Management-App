d2d299f54854daf8dbfb6c2d44ff22884a8fa4f9
Kabya17 <jahindaiyan707@gmail.com>
2024-09-26 19:57:01 +0600

    lkj

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,8 @@ const app = express();
 app.use(cors({
   origin: 'https://user-management-app-frontend.vercel.app', 
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'], 
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true, 
 }));
 
 app.use(express.json());
@@ -15,7 +16,7 @@ app.use('/api', userRoutes);
 app.get('/', (req, res) => {
   res.send('Welcome to the User Management API! Use /api/register, /api/login, etc. to interact with the API.');
 });
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 
