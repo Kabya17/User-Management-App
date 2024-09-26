782b17f5ae4d4f8d6d0b766572df352a90aeb1f9
Kabya17 <jahindaiyan707@gmail.com>
2024-09-26 12:05:43 +0600

    Your commit message here

--- backend/config/dbConfig.js
+++ backend/config/dbConfig.js
@@ -1,10 +1,17 @@
 const mysql = require('mysql2');
+const fs = require('fs'); 
 require('dotenv').config();
+
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: true, 
+    ca: fs.readFileSync('./config/aiven-ca-cert.pem'),
+  },
 });
 
 db.connect((err) => {
@@ -12,8 +19,8 @@ db.connect((err) => {
     console.error('Error connecting to MySQL:', err);
     process.exit(1);
   } else {
-    console.log('Connected to MySQL');
+    console.log('Connected to MySQL on Aiven');
   }
 });
-module.exports = db;
 
+module.exports = db;
